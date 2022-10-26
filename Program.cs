fb6d709ba18b2aca36fa1c7a393f11263830c28c
tarundighaliya <86654334+tarundighaliya@users.noreply.github.com>
2022-10-26 10:21:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Write a C program to print all even numbers between 1 to 100
+using System;
+namespace Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 1;
+            int b = 1;
+            for(int i = 1; i <= 100; i++)
+            {
+                b=i% 2;
+                if (b == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
